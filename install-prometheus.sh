4c1695c2b3635e9df6a9d1ca1e11d059b17b5514
Tony Yin <14_ykl@tongji.edu.cn>
2019-05-26 00:16:34 +0800

    update install script

--- install-prometheus.sh
+++ install-prometheus.sh
@@ -1,7 +1,7 @@
-kubectl create -f manifests-monitoring/
+kubectl create -f /home/sockshop-deployment/manifests-monitoring/
 
 # It can take a few seconds for the above 'create manifests' command to fully create the following resources, so verify the resources are ready before proceeding.
 until kubectl get customresourcedefinitions servicemonitors.monitoring.coreos.com ; do date; sleep 1; echo ""; done
 until kubectl get servicemonitors --all-namespaces ; do date; sleep 1; echo ""; done
 
-kubectl apply -f manifests-monitoring/ # This command sometimes may need to be done twice (to workaround a race condition).
+kubectl apply -f /home/sockshop-deployment/manifests-monitoring/ # This command sometimes may need to be done twice (to workaround a race condition).
