29313ded366805cf15f96938ab194a69b0884deb
tjuyinkanglin <14_ykl@tongji.edu.cn>
2019-05-27 15:00:02 +0800

    modify scripts for weave-scope

--- delete-weave.sh
+++ delete-weave.sh
@@ -1,2 +1,2 @@
-kubectl apply -f /home/sockshop-deployment/manifests-weave/
-kubectl apply -f "https://cloud.weave.works/k8s/scope.yaml?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+kubectl delete -f /home/sockshop-deployment/manifests-weave/
+kubectl delete -f "https://cloud.weave.works/k8s/scope.yaml?k8s-version=$(kubectl version | base64 | tr -d '\n')"
